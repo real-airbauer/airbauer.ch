3767dee6d6738c031a8556cc036a154dc92988c8
real-airbauer <airbauer2@gmail.com>
2023-12-20 21:34:16 +0100

    !

--- src/utils/funcs.ts
+++ src/utils/funcs.ts
@@ -143,10 +143,10 @@ export const argTab = (
   // 8) if input is 'projects go '
   else if (_.startsWith(inputVal, "projects go ")) {
     [
-      "1.Sat Naing's Blog",
-      "2.Haru Fashion",
-      "3.Haru API",
-      "4.AstroPaper Blog Theme",
+      "1.Terminal Portfolio",
+      "2.noAfD.world",
+      "3.Birthday Bot",
+      "4.Blog",
     ].forEach(t => {
       hintsCmds = [...hintsCmds, t];
     });
